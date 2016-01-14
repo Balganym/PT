670d6ed9e4a827feed8528e908df58c599f85712
Balganym Tulebayeva <Balganym Tulebayeva>
2016-01-14 22:24:45 +0600

    changed files

--- Operator.cs
+++ Operator.cs
@@ -6,38 +6,55 @@ using System.Threading.Tasks;
 
 namespace first
 {
-    class large
+    class complex
     {
-        public long a;
-        public large(long a)
+        public int a, b;
+
+        public complex(int a, int b)
         {
             this.a = a;
+            this.b = b;
         }
 
-        public static large operator +(large c1, large c2)
+        public static complex operator +(complex c1, complex c2)
         {
-            large c3 = new large(c1.a + c2.a);
+            complex c3 = new complex(c1.a + c2.a, c1.b + c2.b);
             return c3;
         }
 
-        public override string ToString()
+        public static complex operator -(complex c1, complex c2)
         {
-            return a + "" ;
+            complex c3 = new complex(c1.a - c2.a, c1.b - c2.b);
+            return c3;
         }
 
+        public static complex operator *(complex c1, complex c2)
+        {
+            complex c3 = new complex(c1.a * c2.a, c1.b * c2.b);
+            return c3;
+        }
+
+
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
 
-            large d = new large(7777777777777777);
-            large k = new large(5555555555555555);
-            large f = k + d;
+
+            complex c = new complex(5, 4);
+            complex d = new complex(11, 5);
+            complex w = c + d;
+            complex f = c - d;
+            complex k = c * d;
+            Console.WriteLine(w);
             Console.WriteLine(f);
-            Console.ReadKey(); 
-
-
-        }   
+            Console.WriteLine(k);
+            Console.ReadKey();
+        }
     }
 }
