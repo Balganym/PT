374db0411212b05824e60c545f76172a09927808
Balganym Tulebayeva <Balganym Tulebayeva>
2016-01-15 11:48:22 +0600

    calculator

---
+++ project1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+  
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+             double a, b;
+             string name;
+             char ch;
+
+             Console.WriteLine("Enter your name:");
+             name = Console.ReadLine();
+             Console.WriteLine("Hello {0}!", name);
+             Console.WriteLine("Give me 2 numbers");
+             a = Convert.ToDouble(Console.ReadLine());
+             b = Convert.ToDouble(Console.ReadLine());
+
+             Console.WriteLine("Choose operation: + - / *");
+             ch = Convert.ToChar(Console.ReadLine());
+
+             if (ch == '+')
+             {
+                 Console.WriteLine("a + b = {0}", a + b);
+             }
+
+             if (ch == '-')
+             {
+                 Console.WriteLine("a - b = {0}", a - b);
+             }
+
+             if (ch == '*')
+             {
+                 Console.WriteLine("a * b = {0}", a * b);
+             }
+
+             if (ch == '/')
+             {
+                 Console.WriteLine("a / b = {0}", a / b);
+             }
+             Console.WriteLine("OK");
+
+          
+            Console.ReadKey(); 
+        }
+    }
+        
+}
