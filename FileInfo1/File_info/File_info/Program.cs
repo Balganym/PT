383f0b987a3c7fb6f258d02c52d1952b89394621
Balganym Tulebayeva <Balganym Tulebayeva>
2016-01-22 00:50:45 +0600

    fileInfo 1

---
+++ FileInfo1/File_info/File_info/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileInfo file = new FileInfo(@"C:\Users\Useer\Desktop\far2.0\lab #10.1 ");
+            if (file.Exists)
+            {
+                Console.WriteLine("File name {0}, File fullname {1}", file.Name, file.FullName);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
