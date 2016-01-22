e6b5b623df3ebd87a1f8c2611a07244188866898
Balganym Tulebayeva <Balganym Tulebayeva>
2016-01-22 15:30:19 +0600

    prime numbers

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,41 +6,31 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
-    class Complex
-    {
-        public int a, b;   // создаем комплексное число (а/b)
-
-        public Complex (int a, int b)   // создаем конструктор 
-        {
-            this.a = a; // указываем 
-            this.b = b;
-        }
-
-        public static Complex operator +(Complex n1, Complex n2) // пишим оператор +, чтобы вне класса могли суммировать комплексные числа 
-        {
-            Complex sum = new Complex(n1.a + n2.a, n1.b + n2.b);
-            return sum;
-        }
-
-        public override string ToString() // переопределяем нашу строку (пишим то,что нужно возвращать )
-        {
-            return a + "/" + b;
-        }
-        
-    }
+  
     class Program
     {
         static void Main(string[] args)
         {
-            int num1, num2, num3, num4;  // создаю переменные
-            num1 = Int32.Parse(Console.ReadLine()); 
-            num2 = Int32.Parse(Console.ReadLine());
-            num3 = Int32.Parse(Console.ReadLine());
-            num4 = Int32.Parse(Console.ReadLine());
 
-            Complex first_complex = new Complex(num1, num2); // обращаюсь к классу(передаю переменные)(здесь num1 = n1.a, num2 = n1.b и т.д.)
-            Complex second_complex = new Complex(num1, num2);
-            Complex sum = first_complex + second_complex; // вызываю оператор + 
+            string[] token = Console.ReadLine().Split(); // вводим числа через пробел, кака строка            
+            for (int i = 0; i < token.Length; i++)
+            {
+                int cnt = 0; // задаем счетчик (который будет считать сколько делителей есть у данного числа)
+                int a = int.Parse(token[i]); // каждый элемент строки запмсываем как тип integer             
+
+                for (int j = 1; j <= a; j++) // пробегаемся по циклу от 1 до данного числа (то есть, делим данное число на все числа до самого числа)
+                {
+                    if (a % j == 0) // если данное число делится на кокое то число 
+                    {
+                        cnt++; // прибавляем нашеу счетчику 1
+                    }
+                }
+                if (cnt < 3) // если счетчик, то есть число делителей меньше 3 (то есть делители 1 и само число)
+                {
+                    Console.Write("{0} ", a); // выводим это число (простое число)                    
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
