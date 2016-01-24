46c7966ff9c819970c78cfd47986314ae8c4da6f
Balganym Tulebayeva <Balganym Tulebayeva>
2016-01-24 12:41:12 +0600

    class Student

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,30 +6,35 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
-  
+  class Student
+    {
+        public string name;
+        public string surname;        
+        public Double GPA;
+
+        public Student (string _name, string _surname, Double _gpa)
+        {
+            this.name = _name;
+            this.surname = _surname;
+            this.GPA = _gpa;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + surname + " " + GPA; 
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            Double gpa = Convert.ToDouble(Console.ReadLine());
+            Student information = new Student(name, surname, gpa);
 
-            string[] token = Console.ReadLine().Split(); // вводим числа через пробел, кака строка            
-            for (int i = 0; i < token.Length; i++)
-            {
-                int cnt = 0; // задаем счетчик (который будет считать сколько делителей есть у данного числа)
-                int a = int.Parse(token[i]); // каждый элемент строки запмсываем как тип integer             
-
-                for (int j = 1; j <= a; j++) // пробегаемся по циклу от 1 до данного числа (то есть, делим данное число на все числа до самого числа)
-                {
-                    if (a % j == 0) // если данное число делится на кокое то число 
-                    {
-                        cnt++; // прибавляем нашеу счетчику 1
-                    }
-                }
-                if (cnt < 3) // если счетчик, то есть число делителей меньше 3 (то есть делители 1 и само число)
-                {
-                    Console.Write("{0} ", a); // выводим это число (простое число)                    
-                }
-            }
+            Console.WriteLine(information);
             Console.ReadKey();
         }
     }
