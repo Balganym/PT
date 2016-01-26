d42d3470d3a4c8fb5a4b9c846354ff5543eb13b9
Balganym Tulebayeva <Balganym Tulebayeva>
2016-01-26 21:28:30 +0600

    fileInfo 1

---
+++ FileInfo/Lecture1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Lecture1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Useer\Desktop\far2.0\Balganym");
+            int cnt = Search(d);
+            Console.WriteLine(cnt);
+            Console.ReadKey();
+        }
+        static int Search(DirectoryInfo d)
+        {
+            FileInfo[] files = d.GetFiles();
+            int cnt_files = files.Length;
+            return cnt_files;
+        }
+    }
+}
