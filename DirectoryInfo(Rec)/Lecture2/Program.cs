d40cc2c6eec481fd2e12500486f98b5627431e3f
Balganym Tulebayeva <Balganym Tulebayeva>
2016-01-26 21:43:05 +0600

    Files in directory(recursion)

---
+++ DirectoryInfo(Rec)/Lecture2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Lecture1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Useer\Desktop\far2.0\Balganym");
+            int cnt = Search(d);
+            Console.WriteLine(cnt);
+            Console.ReadKey();
+        }
+        static int Search(DirectoryInfo d)
+        {
+            FileInfo[] files = d.GetFiles();
+            int cnt_files = files.Length;
+
+            DirectoryInfo[] directories = d.GetDirectories();
+            foreach(DirectoryInfo  directory in directories){
+                cnt_files += Search(directory);
+            }
+            return cnt_files;
+        }
+    }
+}
