4efc69053a270fbb4594b022106e19dcbc7b97cd
Balganym Tulebayeva <Balganym Tulebayeva>
2016-02-07 19:32:12 +0600

    Far

---
+++ Far/Far/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FarManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Useer\Desktop\far2.0";
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            List<FileSystemInfo> items = new List<FileSystemInfo>();
+            items.AddRange(dir.GetDirectories());
+            items.AddRange(dir.GetFiles());
+
+            int index = 0;
+
+            while (true)
+            {
+                for (int i = 0; i < items.Count; ++i)
+                {
+                    if (i == index)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                    }
+                    if (items[i].GetType() == typeof(FileInfo))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                    Console.WriteLine(items[i].Name);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+
+
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                switch (pressedKey.Key)
+                {
+                    
+                    case ConsoleKey.UpArrow:
+                        if (index > 0) index--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (index < items.Count - 1) index++;
+                        break;
+                    case ConsoleKey.Enter:
+                        if (items[index].GetType() == typeof(DirectoryInfo))
+                        {
+                            
+                            path = items[index].FullName;
+                            dir = new DirectoryInfo(path);
+                            items.Clear();
+                            items.AddRange(dir.GetDirectories());
+                            items.AddRange(dir.GetFiles());
+                            index = 0;
+                        }
+                        break;                    
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
