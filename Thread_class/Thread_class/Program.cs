2c81ebee415ce1e5ffe84b368da11886ce54b3e1
Balganym Tulebayeva <Balganym Tulebayeva>
2016-03-14 22:17:08 +0600

    my thread with class

---
+++ Thread_class/Thread_class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+            ThreadClass t = new ThreadClass(a);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Thread_class/Thread_class/ThreadClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread_class
+{
+    class ThreadClass
+    {
+       
+        public ThreadClass(int a)
+        {
+
+            Thread[] t = new Thread[a];
+            for (int i = 0; i < a; i++)
+            {
+                t[i] = new Thread(f);
+                t[i].Name = "my thread " + i;
+                t[i].Start();
+                t[i].Join();
+                Thread.Sleep(500);               
+            }
+        }
+
+        public void f()
+        {
+            for (int i = 1; i<6; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+                Thread.Sleep(1000);
+            }        
+        }
+    }
+}
